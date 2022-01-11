a7bcea24600f28ccda9d099c81dccb3356948104
Олег <osumsky@gmail.com>
2022-01-11 11:58:08 +0200

    added JOIN

--- fm_test.session.sql
+++ fm_test.session.sql
@@ -1,21 +1,46 @@
-SELECT sum("quantity")
-FROM phones_to_orders;
+-- 
+SELECT u."email",
+  concat("firstName", ' ', "lastName") AS FIO
+FROM "users" AS u
+  JOIN "orders" AS o ON o."userId" = u."id"
+  JOIN "phones_to_orders" AS pto ON pto."orderId" = o."id"
+  JOIN "phones" AS p ON p."id" = pto."phoneId"
+WHERE p."brand" ILIKE 'iphone'
+GROUP BY u."email",
+  u."firstName",
+  u."lastName"
+ORDER BY FIO; 
 
-SELECT sum("quantity")
-FROM phones;
+--
+SELECT concat("firstName", ' ', "lastName") AS FIO,
+  count(*) AS amount
+FROM "users" AS u
+  JOIN "orders" AS o ON o."userId" = u."id"
+GROUP BY u."id"
+HAVING count(*) > 3
+ORDER BY amount; 
 
+--
+SELECT concat("firstName", ' ', "lastName") AS FIO,
+  count(*) AS amount
+FROM "users" AS u
+  JOIN "orders" AS o ON o."userId" = u."id"
+GROUP BY u."id"
+HAVING count(*) < 3
+ORDER BY amount;
 
-SELECT avg("price")
-FROM phones;
+--
+SELECT o."id", u."email" 
+FROM "users" AS u
+  JOIN "orders" AS o ON o."userId" = u."id"
+  JOIN "phones_to_orders" AS pto ON pto."orderId" = o."id"
+  JOIN "phones" AS p ON p."id" = pto."phoneId"
+WHERE p."id" = 33;
 
-SELECT "brand", avg("price")
-FROM phones
-GROUP BY "brand";
-
-
-SELECT "brand", count(*)
-FROM phones
-GROUP BY "brand"
-ORDER BY "brand";
-
-
+--
+SELECT p."brand", p."model", sum(pto."quantity") as amount
+FROM "phones" AS p
+  JOIN "phones_to_orders" AS pto ON pto."phoneId" = p."id"
+GROUP BY p."id"
+ORDER BY amount
+LIMIT 1;
